f80a34e08ee8402e099658f913133817cfb29971
“Arunesh <as8175802986@gmail.com>
2021-11-29 22:35:06 +0530

    [Arunesh]Added: code for User registeration Problem

---
+++ Regex-Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("----------Welcome to User Registration Program----------");
+            Console.WriteLine("\n Please enter user's First Name--");
+            string firstName = Console.ReadLine();
+            RegularExp.FirstName(firstName);                                 //taking user input and passing it in first name method of regular exp class
+           // string lastName = Console.ReadLine();
+            //RegularExp.LastName(lastName);                                  //taking user input and passing it in last name method of regular exp class
+            //Console.WriteLine("\n Please enter user's Email Id's--");
+            //string email = Console.ReadLine();
+            //RegularExp.Email(email);                                        //email entry
+            //Console.WriteLine("\n Please enter user's MobileNumber--");
+            //string mobile = Console.ReadLine();
+            //RegularExp.Mobile(mobile);
+
+           
+
+        }
+    }
+}
---
+++ Regex-Demo/RegularExp.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Demo
+{
+    class RegularExp
+    {
+        public static void FirstName(string name)                   //declaring the static meythod to take user's first name as entry
+        {
+            string firstName = "^[A-Z]{1}[a-zA-Z]{2,}$";            //this regular exp shows that first letter must be capital, and minimum letter must be 3
+            Regex regex = new Regex(firstName);
+            if (regex.IsMatch(name))                               //calling the Regex class's functions
+            {
+                Console.WriteLine(name + " is valid. Proceed to entering the Last Name");
+            }
+            else
+                Console.WriteLine(name + " is invalid. Please check your name and start with capital letter.");
+        }
+
+
+        public static void LastName(string name)                 //declaring the static meythod to take user's Last name as entry
+        {
+            string lastName = "^[A-Z]{1}[a-zA-Z]{2,}$";          //this regular exp shows that first letter must be capital, and minimum letter must be 3
+            Regex regex = new Regex(lastName);
+            if (regex.IsMatch(name))                             //calling the Regex class's functions
+            {
+                Console.WriteLine(name + " is valid");
+            }
+            else
+                Console.WriteLine(name + " is invalid. Please check your name and start with capital letter.");
+        }
+        public static void EmailArr(string[] name) //to check the array of email id's if needed.
+        {
+            string email = @"^[a-zA-Z0-9]+[+-._]?[a-zA-Z0-9]*[+-._]?[a-zA-Z0-9]+@[a-zA-Z0-9]+[.]{1}[a-zA-Z]{2,3}[.]?[a-zA-Z]{0,3}$";
+            //the regular exp email sample.
+            //+ means one or more. *- zero or more. ?- zero or 1 
+
+            Regex regex = new Regex(email);            //creating a regex named obj with regular exp email.
+            foreach (var word in name)
+            {
+                if (regex.IsMatch(word))
+                {
+                    Console.WriteLine(word + " is valid.");
+                }
+                else
+                    Console.WriteLine(word + " is invalid.");
+            }
+
+        }
+        public static void Email(string name)
+        {
+            string email = @"[a-zA-Z0-9]+[+-._]?[a-zA-Z0-9]*[+-._]?[a-zA-Z0-9]+@[a-zA-Z0-9]+[.]{1}[a-zA-Z]{2,3}[.]?[a-zA-Z]{0,3}$";
+            //the regular exp email sample.
+            //+ means one or more. *- zero or more. ?- zero or 1 
+
+            Regex regex = new Regex(email);            //creating a regex named obj with regular exp email.                        
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid. Proceed to enterring your mobile number.");
+            }
+            else
+                Console.WriteLine(name + " is invalid.");
+        }
+        public static void Mobile(string name)
+        {
+            string mobile = "^([0-9]{2}[ ][0-9]{10})|([0-9]{3}[ ][0-9]{10})$";         //here both country codes with 2 or 3 numbers will be valid. other than that invalid.
+            Regex regex = new Regex(mobile);                                           //creating a regex named obj with regular exp mobile number.                        
+
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid. Proceed to enterring your password.");
+            }
+            else
+                Console.WriteLine(name + " is invalid.");
+        }
+    }
+}
